6d4a4020b65bec4b07486d2c16c261697c07cb78
fanhe <hee.fan@outlook.com>
2025-08-22 10:33:07 +0800

    refactor(telescope): migrate keymaps to config with builtin API

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,13 +4,13 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
-    keys = {
-        { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
-        { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
-        { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find buffers' },
-        { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Help tags' },
-    },
     config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help' })
+        
         require('telescope').setup({
             defaults = {
                 file_ignore_patterns = { "node_modules", ".git" },
