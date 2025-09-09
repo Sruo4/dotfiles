a2d0bcdf612e98751775230d97a6a8011b8b8644
fanhe <hee.fan@outlook.com>
2025-09-09 16:50:15 +0800

    feat: change tokyonight theme to night

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,10 +2,15 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-        require("tokyonight").setup({
-            style = "night",
-        })
+    opts = {
+        style = "night", -- tokyonight theme
+    },
+    config = function(_, opts)
+        -- 设置背景为 dark (对于 night, storm 等深色主题是好习惯)
+        vim.opt.background = "dark"
+
+        -- 加载配置并应用色彩主题
+        require("tokyonight").setup(opts)
         vim.cmd.colorscheme("tokyonight")
     end,
 }
