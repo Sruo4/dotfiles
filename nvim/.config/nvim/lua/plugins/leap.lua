ddbeae0e588a0ace899a24a947a7fc35b8d8c25a
fanhe <hee.fan@outlook.com>
2025-11-04 23:09:58 +0800

    feat(nvim): complete XDG configuration migration with plugin updates
    - Move entire nvim config to nvim/.config/nvim/ following XDG structure
    - Enhance snacks.nvim with dashboard enabled
    - Simplify leap.nvim configuration by removing default mappings
    - Continue XDG + stow migration

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,7 +1,4 @@
 return {  
   'ggandor/leap.nvim',  
   dependencies = {'tpope/vim-repeat'},  
-  config = function()  
-    require('leap').set_default_mappings()  
-  end  
 }
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
+    dashboard = { enabled = true },
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
